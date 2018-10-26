8a2dceca261d1928f73564493b6c7ec4c443ef3f
Sniffy1988 <Sniffy1988@gmail.com>
2018-10-26 18:23:23 +0300

    rework to validate entered value and move checks to method of class;

--- src/components/addForm/ToDoAddForm.js
+++ src/components/addForm/ToDoAddForm.js
@@ -4,58 +4,78 @@ import './styles.scss';
 
 export default class ToDoAddForm extends Component {
     static propTypes = {
-        onAdd : PropTypes.func
+        onAdd: PropTypes.func
     };
 
-    state ={
+    state = {
         todo: ''
     };
 
     onEnterTodo = ({target}) => {
         let {value} = target;
-        if(value.indexOf(' ') !== 0 && value !== '') {
-            this.setState({
-                todo: value,
-                isError: false,
-                errorMsg: null
-            })
-        } else {
-            this.setState({
-                todo: value,
-                isError: true,
-                errorMsg: `ToDo shouldn't be empty or starts with space`
-            })
-        }
+        this.validateToDo(value);
+        this.setState({
+            todo: value
+        });
     };
 
     isDisabled = () => {
         return this.state.isError;
     };
 
-    sendToDo = ()=> {
-        let result = this.props.onAdd(this.state.todo);
-        if(result.result) {
+    validateToDo = (value) => {
+        if(value.indexOf(' ') === 0 || value === '') {
             this.setState({
-                todo: '',
-                isError: false
-            })
+                isError: true,
+                errorMsg: `ToDo shouldn't be empty or starts with space`
+            });
+            return false;
         } else {
             this.setState({
-                isError : true,
-                errorMsg: result.msg
-            })
+                isError: false,
+                errorMsg: null
+            });
+            return true;
         }
     };
 
-    render (){
+    sendToDo = () => {
+        if (this.state.todo === '') {
+            this.setState({
+                isError: true,
+                errorMsg: `ToDo shouldn't be empty or starts with space`
+            });
+        } else {
+            let validate = this.validateToDo(this.state.todo);
+            if(validate) {
+                let result = this.props.onAdd(this.state.todo);
+                if (result.result) {
+                    this.setState({
+                        todo: '',
+                        isError: false
+                    })
+                } else {
+                    this.setState({
+                        isError: true,
+                        errorMsg: result.msg
+                    })
+                }
+            }
+        }
+    };
+
+    render() {
         return (
             <section className={'form-inline add-form'}>
                 <div className="form-group">
-                    <input type="text" value={this.state.todo} onChange={this.onEnterTodo} placeholder={'Enter todo'} className="form-control"/>
-                    <button className={'btn btn-primary btn-add'} onClick={this.sendToDo} disabled={this.isDisabled()}>Add</button>
+                    <input type="text" value={this.state.todo} onChange={this.onEnterTodo} placeholder={'Enter todo'}
+                           className="form-control"/>
+                    <button className={'btn btn-primary btn-add'} onClick={this.sendToDo}
+                            disabled={this.isDisabled()}>Add
+                    </button>
                 </div>
                 {this.state.isError &&
-                    <div className={'form-text error'}>{this.state.errorMsg}</div>
+                <div className={'form-text error'}>{this.state.errorMsg}</div>
                 }
             </section>
         )
